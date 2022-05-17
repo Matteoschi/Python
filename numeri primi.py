0babfbdcb7e37412a741c2344978708bfcdd8104
Matte <94646702+Matteoschi@users.noreply.github.com>
2022-05-17 17:49:40 +0200

    Update numeri primi.py

--- numeri primi.py
+++ numeri primi.py
@@ -3,6 +3,8 @@ Numero=int(input("Indicare numero"))
 for i in range(2, Numero):
     if (Numero % i) == 0:
         Riv = True
+    else:
+        Riv = False
 
 if Riv:
     print(Numero, "Non è primo")
