274986603c496791144c7a8d9aeb75d9c6e47b55
Matte <94646702+Matteoschi@users.noreply.github.com>
2022-05-19 20:01:38 +0200

    Update numeri primi.py

--- numeri primi.py
+++ numeri primi.py
@@ -1,18 +1,14 @@
 import time
 Numero = int(input("Indicare numero "))
-N = True
+primo = True
 for i in range(2, Numero):
     if (Numero % i) == 0:
         primo = False
-for i in range(2, Numero):
-    if (Numero % i) > 0:
-        b = True
-        a = False
 
-if N: 
+
+if primo: 
  print (Numero," è un numero primo")
 else:
  print (Numero," non è un numero primo")
 
 time.sleep(10)
-
