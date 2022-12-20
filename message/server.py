0db1fd0b1e9e39ba76aaa05011ab6043eba3ad5c
Matte <94646702+Matteoschi@users.noreply.github.com>
2022-12-20 22:46:35 +0100

    Add files via upload

---
+++ message/server.py
@@ -0,0 +1,13 @@
+import socket
+server=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(("localhost",9999))
+server.listen()
+client,addr=server.accept()
+done=False
+while not done:
+    msg=(client.recv(1024).decode('UTF-8'))
+    if msg =="stop":
+        done=True
+    else:
+        print(msg)
+    client.send(input("messaggio: ").encode("UTF-8"))
